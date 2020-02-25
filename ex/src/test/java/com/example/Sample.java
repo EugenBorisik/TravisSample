3377cbb2685660f313d21d25cf2873b0370380d6
EugenBorisik <eugenborisik@coherentsolutions.com>
2020-02-25 21:53:49 +0300

    Initial commit

---
+++ ex/src/test/java/com/example/Sample.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+
+  @Test
+  public void sampleTest() {
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://mail.ru");
+  }
+}
