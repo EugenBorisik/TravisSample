4c802002cd80a0cb7c624230481355fe95c3b3ab
EugenBorisik <eugenborisik@coherentsolutions.com>
2020-02-25 22:31:28 +0300

    upd

--- src/test/java/com/example/Sample.java
+++ src/test/java/com/example/Sample.java
@@ -2,13 +2,16 @@ package com.example;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sample {
 
   @Test
   public void sampleTest() {
-//    WebDriver driver = new ChromeDriver();
-//    driver.get("https://mail.ru");
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://mail.ru");
+    driver.close();
     Assertions.assertEquals(4, 8 / 2);
   }
 }
